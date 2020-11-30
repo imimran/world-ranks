511a019847bc2120a7814659e3ea782df74a0398
imimran <engr.aih@gmail.com>
2020-11-30 17:47:02 +0600

    apply country Search

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,48 @@
-import Head from 'next/head'
-import Layout from '../components/Layout/Layout'
-import styles from '../styles/Home.module.css'
-import SearchInput from '../components/SearchInput/SearchInput'
+import Head from "next/head";
+import { useState } from "react";
+import Layout from "../components/Layout/Layout";
+import styles from "../styles/Home.module.css";
+import SearchInput from "../components/SearchInput/SearchInput";
 import CountriesTable from "../components/CountriesTable/CountriesTable";
 
 export default function Home({ countries }) {
   //console.log(countries)
-  return <Layout>
-    <di className={styles.counts}> Found {countries.length} countries</di>
-  <SearchInput placeholder="Filter by Name,Region or SubRegion"/>
+  const [keyword, setKeyword] = useState("");
 
-  <CountriesTable countries={countries} />
-  </Layout>
-}
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
 
+  const onInputChange = (e) =>{
+    e.preventDefault()
 
-export const getStaticProps = async () =>{
-  const res = await fetch("https://restcountries.eu/rest/v2/all");
-  const countries = await res.json()
-
-  return{
-    props:{
-      countries,
-    }
+    setKeyword(e.target.value.toLowerCase())
   }
+
+
+  return (
+    <Layout>
+      <di className={styles.counts}> Found {countries.length} countries</di>
+      <SearchInput
+        placeholder="Filter by Name,Region or SubRegion"
+        onChange={onInputChange}
+      />
+
+      <CountriesTable countries={filteredCountries} />
+    </Layout>
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
